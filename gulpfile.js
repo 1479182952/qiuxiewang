6d795c5b5e6eb7ce25039b2373a482f0ec43eddc
pengwenjiang <865371572@qq.com>
2017-08-25 09:41:43 +0800

    初始化项目目录结构

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp=require("gulp"),
+    uglify=require("gulp-uglify");
+gulp.task("uglify",function(){
+	  gulp.src("js/*.js")
+	      .pipe(uglify())
+		  .pipe(gulp.dest("dist/js")) ;
+});	
+	 
